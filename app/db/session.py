ea55c2ab8293c7002a7909242b0930de7ab4cadc
Dexter2038 <116676321+Dexter2038@users.noreply.github.com>
2025-09-27 00:30:15 +0300

    refactor(db session): change from sessionmaker to async_sessionmaker for better typing support

--- app/db/session.py
+++ app/db/session.py
@@ -1,10 +1,9 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from contextlib import asynccontextmanager
 from app.config import settings
 
 engine = create_async_engine(settings.db_url, echo=True)
-async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
 @asynccontextmanager
